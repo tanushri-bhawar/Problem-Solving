d18c50c513290c71ad1d7b0f30802e7b985a5bb5
tanushri-bhawar <tanubhawar@gmail.com>
2026-07-21 17:35:31 +0530

    Time: 0 ms (100%), Space: 9.2 MB (4.18%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,14 @@
+int addDigits(int num) {
+    while(num>9)
+    {
+        int sum=0;
+        while(num)
+        {
+            sum+=num%10;
+            num/=10;
+        }
+        num=sum;
+    }
+
+    return num;
+}
