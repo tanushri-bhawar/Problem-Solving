07c475e67a4fcc3c46b1894af1c1f237b9a652a1
tanushri-bhawar <tanubhawar@gmail.com>
2026-07-29 12:53:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13.19%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int m=min(a,b);
+        int cnt=0;
+        for(int i=1;i<=m;i++)
+        {
+            if(a%i ==0 && b%i==0)
+            cnt++;
+        }
+        return cnt;
+    }
+};
