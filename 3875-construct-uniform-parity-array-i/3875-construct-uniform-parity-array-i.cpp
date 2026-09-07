5587a8704a3f182f6416926ea5dfe413b130ded3
tanushri-bhawar <tanubhawar@gmail.com>
2026-09-07 23:07:47 +0530

    Time: 0 ms (100%), Space: 30.4 MB (30%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
        // vector<int>ans;
+
+       //if all odd->true
+       //if all even-> true
+       //if mix then can be transform to odd 
         return true;
 
     }
