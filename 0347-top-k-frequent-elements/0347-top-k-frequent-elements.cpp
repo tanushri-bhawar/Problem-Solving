1dbf66dd3b760699f31042138ae4c816b4a2895f
tanushri-bhawar <tanubhawar@gmail.com>
2026-09-09 13:16:53 +0530

    Time: 4 ms (34.39%), Space: 18.1 MB (51.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int maxx(map<int,int>&mp)
-    {
-        //int key=0;
-        auto max=mp.begin()->first;
-        for(auto x:mp)
-        {
-            if(mp[max]<x.second)max=x.first;
-        }
-        mp[max]=0;
-        return max;
-    }
+
+    // int maxx(map<int,int>&mp)
+    // {
+    //     //int key=0;
+    //     auto max=mp.begin()->first;
+    //     for(auto x:mp)
+    //     {
+    //         if(mp[max]<x.second)max=x.first;
+    //     }
+    //     mp[max]=0;
+    //     return max;
+    // }
 
     vector<int> topKFrequent(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());//sorted element
@@ -21,12 +22,29 @@ public:
             mp[nums[i]]++; //frequency
         }
         
+        //1st way
+        // while(k>0)
+        // {
+        //     int max=maxx(mp);
+        //     ans.push_back(max);
+        //     k--;
+        // }
+
+
+        //2nd way
+        priority_queue<pair<int, int>> pq;
+        for(auto it : mp)
+        {
+            pq.push({it.second, it.first});
+        }
+        
         while(k>0)
         {
-            int max=maxx(mp);
-            ans.push_back(max);
+            ans.push_back(pq.top().second);
+            pq.pop();
             k--;
         }
+
         return ans;
     }
 };
