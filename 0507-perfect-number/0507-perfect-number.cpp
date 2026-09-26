53571a31c93a6bfb831c13a81c8b2f9ba6a267f1
tanushri-bhawar <tanubhawar@gmail.com>
2026-09-26 16:49:02 +0530

    Time: 1039 ms (53.05%), Space: 7.6 MB (99.45%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    bool checkPerfectNumber(int num) {
+    bool checkPerfectNumber(int num) 
+    {
         int sum=0;
         for(int i=1;i<=(num/2);i++)
         {
-            if(num%i==0)    
-                sum+=i;
+            if(num%i==0) sum+=i;
         }
-        if(sum==num)
-            return true;
+        if(sum==num) return true;
         return false;
     }
 };
